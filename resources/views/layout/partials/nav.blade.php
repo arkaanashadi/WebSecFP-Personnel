62022b3be1299f15fd2759b340431f7b4fc24096
arkaanashadi <arkaan.ashadi@gmail.com>
2019-04-03 20:39:20 +0700

    Switched to secure_url

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <header class="masthead mb-auto sticky-top">
     <nav class="navbar navbar-expand-md navbar-dark">
-        <a class="navbar-brand" href="{{ url('.') }}"><img src="{{ secure_asset('img/logo.png') }}"></a>
+        <a class="navbar-brand" href="{{ secure_url('.') }}"><img src="{{ secure_asset('img/logo.png') }}"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -10,22 +10,22 @@
             @if(session()->has('id'))
                 @if(session()->has('musicianemail'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('musician') }}">{{ Session::get('username') }}</a>
+                    <a class="nav-link" href="{{ secure_url('musician') }}">{{ Session::get('username') }}</a>
                 </li>
                 @elseif(session()->has('bandemail'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('band') }}">{{ Session::get('bandname') }}</a>
+                    <a class="nav-link" href="{{ secure_url('band') }}">{{ Session::get('bandname') }}</a>
                 </li>
                 @endif
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('signout') }}">Sign Out</a>
+                    <a class="nav-link" href="{{ secure_url('signout') }}">Sign Out</a>
                 </li>
                 @else 
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('signin') }}">Sign In</a>
+                    <a class="nav-link" href="{{ secure_url('signin') }}">Sign In</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('signup') }}">Sign Up</a>
+                    <a class="nav-link" href="{{ secure_url('signup') }}">Sign Up</a>
                 </li>
             @endif
             </ul>
